a909027b81d1e7899d8145eb97a16125cf198213
Kabir192003 <160172047+Kabir192003@users.noreply.github.com>
2026-03-05 01:26:57 +0000

    Update Project2.jsx

--- src/pages/Project2.jsx
+++ src/pages/Project2.jsx
@@ -8,10 +8,10 @@ const Project2 = () => {
 
     // Hardcoded data for Project 1
     const project = {
-        title: 'Eco-Store App',
-        category: 'Mobile App Design',
-        timeline: '3 Months',
-        role: 'Lead Product Designer',
+        title: 'Workhive',
+        category: 'Web-Platform',
+        timeline: '4 Months',
+        role: 'Product Designer',
         overview: 'A unified career platform designed to simplify how users discover jobs, evaluate companies, and build meaningful professional connections. The interface focuses on structured search, assisted filtering, and actionable insights like salary comparisons and relocation costs reducing the cognitive load of switching between multiple platforms. WorkHive is driven by the need to make the job search more transparent, data informed, and efficient for both candidates and recruiters.',
         problem: 'The current job ecosystem is fragmented and overwhelming. Job seekers rely on multiple tools for job discovery, networking, salary research, and company reviews, leading to scattered information and inefficient decision-making. Interfaces are often cluttered with poor categorization, making it difficult to filter relevant opportunities. Recruiters, on the other hand, struggle with inconsistent candidate data, lack of verified insights, and time consuming shortlisting processes.',
         solution: 'A user-centric platform that integrates job search, candidate discovery, company ratings, salary insights, and relocation cost estimation into a single ecosystem. Features like refined filters, assisted search, and structured candidate profiles enable faster and more relevant matching. By combining data transparency with intuitive navigation, WorkHive streamlines the hiring journey helping users make confident decisions and enabling recruiters to efficiently identify and engage the right talent.',
